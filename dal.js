b39f22798060bb491189bb4aa3667d8a7f091714
rodolphonapoleon <rnapoleon@imajine.ht>
2022-05-12 13:43:35 -0400

    added database uri

--- dal.js
+++ dal.js
@@ -1,5 +1,7 @@
 const MongoClient = require("mongodb").MongoClient;
-const url = "mongodb://localhost:27017";
+// const url = "mongodb://localhost:27017";
+const url =
+  "mongodb+srv://napo:12345@ultrabank-database.gpddg.mongodb.net/usersdata?retryWrites=true&w=majority";
 let db = null;
 
 // connect to mongo
--- index.js
+++ index.js
@@ -18,20 +18,20 @@ app.use("/", router);
 
 // create user account
 app.get("/account/create/:name/:email", function (req, res) {
-  //   check if account exists
-  // dal.find(req.params.email).then((users) => {
-  //   // if user exists, return error message
-  //   if (users.length > 0) {
-  //     console.log("User already in exists");
-  //     res.send("User already in exists");
-  //   } else {
-  //   else create user
+    // check if account exists
+  dal.find(req.params.email).then((users) => {
+    // if user exists, return error message
+    if (users.length > 0) {
+      console.log("User already in exists");
+      res.send("User already in exists");
+    } else {
+    // else create user
   dal.create(req.params.name, req.params.email).then((user) => {
     console.log(user);
     res.send(user);
   });
-  //   }
-  // });
+    }
+  });
 });
 
 // login user
