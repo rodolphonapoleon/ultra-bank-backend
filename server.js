dc7b321db90a6e5f3efd8ea8614a69ea0381263f
rodolphonapoleon <rnapoleon@imajine.ht>
2022-05-25 10:07:53 -0400

    removed admin and dal

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express = require("express");
 var app = express();
 var cors = require("cors");
-var dal = require("./dal.js");
+// var dal = require("./dal.js");
 require("dotenv").config();
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.use(cors());
 
@@ -12,99 +12,99 @@ app.get("/", (req, res) => {
   res.send("App running perfectly");
 });
 
-// create user account
-app.get("/account/create/:name/:email", function (req, res) {
-  const idToken = req.headers.authorization;
-  admin
-    .auth()
-    .verifyIdToken(idToken)
-    .then(function (decodedToken) {
-      //create transaction
-      dal.create(req.params.name, req.params.email).then((user) => {
-        res.send(user);
-      });
-    })
-    .catch(function (error) {
-      // console.log("error:", error);
-      res.sendStatus(401).send("Authentication Fail!");
-    });
-});
+// // create user account
+// app.get("/account/create/:name/:email", function (req, res) {
+//   const idToken = req.headers.authorization;
+//   admin
+//     .auth()
+//     .verifyIdToken(idToken)
+//     .then(function (decodedToken) {
+//       //create transaction
+//       dal.create(req.params.name, req.params.email).then((user) => {
+//         res.send(user);
+//       });
+//     })
+//     .catch(function (error) {
+//       // console.log("error:", error);
+//       res.sendStatus(401).send("Authentication Fail!");
+//     });
+// });
 
-// create TRANSACTION
-app.get("/account/createtransaction/:transaction", function (req, res) {
-  const idToken = req.headers.authorization;
-  admin
-    .auth()
-    .verifyIdToken(idToken)
-    .then(function (decodedToken) {
-      //create transaction
-      dal.createTransaction(req.params.transaction).then((transaction) => {
-        res.send(transaction);
-      });
-    })
-    .catch(function (error) {
-      // console.log("error:", error);
-      res.sendStatus(401).send("Authentication Fail!");
-    });
-});
+// // create TRANSACTION
+// app.get("/account/createtransaction/:transaction", function (req, res) {
+//   const idToken = req.headers.authorization;
+//   admin
+//     .auth()
+//     .verifyIdToken(idToken)
+//     .then(function (decodedToken) {
+//       //create transaction
+//       dal.createTransaction(req.params.transaction).then((transaction) => {
+//         res.send(transaction);
+//       });
+//     })
+//     .catch(function (error) {
+//       // console.log("error:", error);
+//       res.sendStatus(401).send("Authentication Fail!");
+//     });
+// });
 
-//find transactions by user email
-app.get("/account/findTransactions/:email", function (req, res) {
-  dal.findTransactions(req.params.email).then((response) => {
-    res.send(response);
-  });
-});
+// //find transactions by user email
+// app.get("/account/findTransactions/:email", function (req, res) {
+//   dal.findTransactions(req.params.email).then((response) => {
+//     res.send(response);
+//   });
+// });
 
-// find one user by email - alternative to find
-app.get("/account/findOne/:email", function (req, res) {
-  dal.findOne(req.params.email).then((user) => {
-    res.send(user);
-  });
-});
+// // find one user by email - alternative to find
+// app.get("/account/findOne/:email", function (req, res) {
+//   dal.findOne(req.params.email).then((user) => {
+//     res.send(user);
+//   });
+// });
 
-// update - deposit/withdraw amount
-app.get("/account/update/:email/:amount", function (req, res) {
-  var amount = Number(req.params.amount);
-  //get idToken from request header
-  const idToken = req.headers.authorization;
-  // verify token
-  admin
-    .auth()
-    .verifyIdToken(idToken)
-    .then(function (decodedToken) {
-      dal.update(req.params.email, amount).then((response) => {
-        res.send(response);
-      });
-    })
-    .catch(function (error) {
-      res.sendStatus(401).send("Authentication Fail!");
-    });
-});
+// // update - deposit/withdraw amount
+// app.get("/account/update/:email/:amount", function (req, res) {
+//   var amount = Number(req.params.amount);
+//   //get idToken from request header
+//   const idToken = req.headers.authorization;
+//   // verify token
+//   admin
+//     .auth()
+//     .verifyIdToken(idToken)
+//     .then(function (decodedToken) {
+//       dal.update(req.params.email, amount).then((response) => {
+//         res.send(response);
+//       });
+//     })
+//     .catch(function (error) {
+//       res.sendStatus(401).send("Authentication Fail!");
+//     });
+// });
 
-//edit Profile
-app.get("/account/edit/:email/:user", function (req, res) {
-  //get idToken from request header
-  const idToken = req.headers.authorization;
-  admin
-    .auth()
-    .verifyIdToken(idToken)
-    .then(function (decodedToken) {
-      console.log("decodedToken:", decodedToken);
-      dal.editProfile(req.params.email, req.params.user).then((response) => {
-        res.send(response);
-      });
-    })
-    .catch(function (error) {
-      res.sendStatus(401).send("Authentication Fail!");
-    });
-});
+// //edit Profile
+// app.get("/account/edit/:email/:user", function (req, res) {
+//   //get idToken from request header
+//   const idToken = req.headers.authorization;
+//   admin
+//     .auth()
+//     .verifyIdToken(idToken)
+//     .then(function (decodedToken) {
+//       console.log("decodedToken:", decodedToken);
+//       dal.editProfile(req.params.email, req.params.user).then((response) => {
+//         res.send(response);
+//       });
+//     })
+//     .catch(function (error) {
+//       res.sendStatus(401).send("Authentication Fail!");
+//     });
+// });
 
-// all accounts
-app.get("/account/all", function (req, res) {
-  dal.all().then((docs) => {
-    res.send(docs);
-  });
-});
+// // all accounts
+// app.get("/account/all", function (req, res) {
+//   dal.all().then((docs) => {
+//     res.send(docs);
+//   });
+// });
 
 app.listen(PORT);
 console.log("Running on port: " + PORT);
